e31f93111f19b37a231dcffd63f1df34732d7198
Kazuya Shida <kazuya.4da@gmail.com>
2015-08-25 13:03:27 +0900

    Refactor ipa

--- app/mod_ipa/controllers.py
+++ app/mod_ipa/controllers.py
@@ -3,16 +3,13 @@
 
 import os, sys, shutil
 import zipfile
-import biplist
 import pytz
-import urlparse
 
 from flask import Blueprint, Flask, url_for, request, render_template, \
                   redirect, escape, Markup, make_response, send_file, \
                   Response, flash, g, jsonify
 from werkzeug import secure_filename
 from datetime import datetime, date, timedelta
-from jinja2 import Environment, FileSystemLoader
 
 from app import app, db
 from app.mod_ipa.models import Ipa
@@ -47,16 +44,11 @@ def create(app_id):
                 os.makedirs(path)
             filepath = os.path.join(path, filename)
             file.save(filepath)
-            bundle_id, display_name, build_version, app_version, error_list = ipa_attribute(filepath, path)
-            plist_path = install_plist(filepath, request.url_root, bundle_id, app_version, display_name)
-
-            print request.url_root
-
-            ipa = Ipa(app_id, filename, filepath, bundle_id, app_version, build_version, display_name, plist_path)
-            ipa.plist_uri = ipa.download_url(request.url_root)
+            ipa = Ipa(app_id, filename, filepath, request.url_root)
             db.session.add(ipa)
             db.session.commit()
 
+            # Remove old ipa, show latest 20
             old_ipalist = Ipa.query.filter_by(app_id=app_id).order_by(Ipa.created_at.desc()).limit(21).all()
             old_ipa = None
             if len(old_ipalist) > 20:
@@ -82,85 +74,3 @@ def allowed_file(filename):
     '''
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-def find(name, path):
-    for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
-
-def ipa_attribute(ipa_path, working_dir):
-    error_list = []
-    unarchive_name = None
-    bundle_id = None
-    display_name = None
-    build_version = None
-    app_version = None
-    try:
-        with zipfile.ZipFile(ipa_path, 'r') as z:
-            for member in z.namelist():
-                if member.startswith('__MACOSX'):
-                    continue
-                z.extract(member, working_dir)
-            unarchive_name = z.namelist()[0]
-    except IOError, e:
-        if os.path.exists(working_dir):
-            shutil.rmtree(working_dir)
-        error_list.append('Can not unarchive %s.' % filename)
-        pass
-
-    if not unarchive_name is None:
-        print unarchive_name
-        path = find('Info.plist', os.path.join(working_dir, unarchive_name))
-        if not path is None:
-            bundle_id = plist(path, 'CFBundleIdentifier')
-            display_name = plist(path, 'CFBundleDisplayName')
-            build_version = plist(path, 'CFBundleVersion')
-            app_version = plist(path, 'CFBundleShortVersionString')
-        else:
-            error_list.append('Not found Info.plist')
-        try:
-            shutil.rmtree(os.path.join(working_dir, unarchive_name))
-        except IOError, e:
-            pass
-    return (bundle_id, display_name, build_version, app_version, error_list)
-
-def plist(filepath, key):
-    try:
-        print filepath
-        plist = biplist.readPlist(os.path.expanduser(filepath))
-    except IOError, e:
-        # TODO: log
-        print e
-        pass
-    value = None
-    if key in plist:
-        value = plist[key]
-    return value
-
-def install_plist(ipa_file, hostname, bundle_id, app_version, display_name):
-    filename = os.path.basename(ipa_file)
-    ipa_dir = os.path.dirname(ipa_file)
-    templates_path = os.path.join(app.config['BASE_DIR'], "app/templates/")
-    env = Environment(loader=FileSystemLoader(templates_path, encoding='utf-8'))
-    template = env.get_template('install.plist')
-
-    upload_dir = os.path.join(app.config['URL_PREFIX'], '/'.join(ipa_dir.split('/')[1:]))
-    download_path = os.path.join(upload_dir, filename)
-    download_url = urlparse.urljoin(hostname, download_path)
-
-    if display_name is None:
-        display_name = filename
-
-    renderer = {
-        'download_url': download_url,
-        'bundle_id': bundle_id,
-        'app_version': app_version,
-        'display_name': display_name
-        }
-    plist = template.render(renderer)
-    plist_path = os.path.join(ipa_dir, 'install.plist')
-    f = open(plist_path, 'w')
-    f.write(plist)
-    f.close
-    return plist_path
-
--- app/mod_ipa/models.py
+++ app/mod_ipa/models.py
@@ -3,8 +3,10 @@
 
 import os, sys, shutil
 import zipfile
+import biplist
 import urlparse
 
+from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 from app import app, db
 
@@ -29,15 +31,20 @@ class Ipa(Base):
 
     app_id        = db.Column(db.Integer, db.ForeignKey('app.id'), nullable = False)
 
-    def __init__(self, app_id, ipa_name, ipa_uri, bundle_id = '', app_version = '', build_version = '', app_name = '', plist = ''):
+    def __init__(self, app_id, ipa_name, ipa_uri, url_root):
+        working_dir = os.path.dirname(ipa_uri)
+        bundle_id, display_name, build_version, app_version, error_list = self.ipa_attribute(ipa_uri, working_dir)
+        plist_path = self.install_plist(ipa_uri, url_root, bundle_id, app_version, display_name)
+
         self.app_id = app_id
         self.ipa_name = ipa_name
         self.ipa_uri = ipa_uri
         self.bundle_id = bundle_id
         self.app_version = app_version
         self.build_version = build_version
-        self.app_name = app_name
-        self.plist_uri = plist
+        self.app_name = display_name
+        self.plist_uri = plist_path
+        self.plist_uri = self.download_url(url_root)
     
     def __repr__(self):
         return '<%s(%r, %r)>' % (self.__class__.__name__, self.id, self.ipa_name)
@@ -58,3 +65,87 @@ class Ipa(Base):
         print plist_url
         url = 'itms-services://?action=download-manifest&url={0}'.format(plist_url)
         return url
+
+    def find(self, name, path):
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                return os.path.join(root, name)
+
+    def plist(self, filepath, key):
+        try:
+            print filepath
+            plist = biplist.readPlist(os.path.expanduser(filepath))
+        except IOError, e:
+            app.logger.error(e)
+            pass
+        value = None
+        if key in plist:
+            value = plist[key]
+        return value
+
+    def ipa_attribute(self, ipa_path, working_dir):
+        error_list = []
+        unarchive_name = None
+        bundle_id = None
+        display_name = None
+        build_version = None
+        app_version = None
+        try:
+            with zipfile.ZipFile(ipa_path, 'r') as z:
+                for member in z.namelist():
+                    if member.startswith('__MACOSX'):
+                        continue
+                    z.extract(member, working_dir)
+                unarchive_name = z.namelist()[0]
+        except IOError, e:
+            if os.path.exists(working_dir):
+                shutil.rmtree(working_dir)
+            error_list.append('Can not unarchive %s.' % filename)
+            pass
+
+        if not unarchive_name is None:
+            print unarchive_name
+            path = self.find('Info.plist', os.path.join(working_dir, unarchive_name))
+            if not path is None:
+                bundle_id = self.plist(path, 'CFBundleIdentifier')
+                display_name = self.plist(path, 'CFBundleDisplayName')
+                build_version = self.plist(path, 'CFBundleVersion')
+                app_version = self.plist(path, 'CFBundleShortVersionString')
+            else:
+                error_list.append('Not found Info.plist')
+            try:
+                shutil.rmtree(os.path.join(working_dir, unarchive_name))
+            except IOError, e:
+                app.logger.error(e)
+                pass
+        return (bundle_id, display_name, build_version, app_version, error_list)
+
+    def install_plist(self, ipa_file, hostname, bundle_id, app_version, display_name):
+        '''
+        Generate plist to install using itms-service
+        '''
+        filename = os.path.basename(ipa_file)
+        ipa_dir = os.path.dirname(ipa_file)
+        templates_path = os.path.join(app.config['BASE_DIR'], "app/templates/")
+        env = Environment(loader=FileSystemLoader(templates_path, encoding='utf-8'))
+        template = env.get_template('install.plist')
+
+        upload_dir = os.path.join(app.config['URL_PREFIX'], '/'.join(ipa_dir.split('/')[1:]))
+        download_path = os.path.join(upload_dir, filename)
+        download_url = urlparse.urljoin(hostname, download_path)
+
+        if display_name is None:
+            display_name = filename
+
+        renderer = {
+            'download_url': download_url,
+            'bundle_id': bundle_id,
+            'app_version': app_version,
+            'display_name': display_name
+            }
+        plist = template.render(renderer)
+        plist_path = os.path.join(ipa_dir, 'install.plist')
+        f = open(plist_path, 'w')
+        f.write(plist)
+        f.close
+        return plist_path
